81adf8a9d5f43dc519acb5da1a4eb9a696c5d79f
Pranay Jha <108553130+pranayjha5666@users.noreply.github.com>
2024-03-04 01:59:47 +0530

    Login With Google Done

--- app/src/main/java/com/example/pulink/Login_Page.java
+++ app/src/main/java/com/example/pulink/Login_Page.java
@@ -1,23 +1,41 @@
 package com.example.pulink;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Login_Page extends AppCompatActivity {
@@ -25,6 +43,13 @@ public class Login_Page extends AppCompatActivity {
     Button loginbtn;
     EditText email_edittext,password_edittext;
     TextView signuptext;
+    ImageView google;
+    FirebaseAuth auth;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    int RC_SIGN_IN=20;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +60,12 @@ public class Login_Page extends AppCompatActivity {
         signuptext=findViewById(R.id.signuptext);
         email_edittext=findViewById(R.id.email_edittext);
         password_edittext=findViewById(R.id.password_edittext);
+        google=findViewById(R.id.google);
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("51839764002-runfu5jnlf1uqjpcjp9820f68f99qk8a.apps.googleusercontent.com").requestEmail().build();
+        gsc= GoogleSignIn.getClient(this,gso);
+        auth=FirebaseAuth.getInstance();
+
+
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,10 +91,84 @@ public class Login_Page extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signinusinggoogle();
+            }
+        });
     }
 
 
-    void loginaccountInFirebase(String email,String password){
+    void signinusinggoogle(){
+        Intent signIntent=gsc.getSignInIntent();
+        startActivityForResult(signIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN){
+            Task<GoogleSignInAccount>task=GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account=task.getResult(ApiException.class);
+                firebaseauth(account.getIdToken());
+
+            } catch (Exception e) {
+                // Log the localized message
+                String errorMessage = e.getLocalizedMessage();
+                Log.e("Localized Error", errorMessage);
+                Toast.makeText(Login_Page.this, errorMessage, Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+
+    public void firebaseauth(String idToken){
+        AuthCredential credential= GoogleAuthProvider.getCredential(idToken,null);
+        auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user=auth.getCurrentUser();
+                    // User created successfully, now store name in Firestore
+                    String userId = user.getUid();
+                    DocumentReference documentReference = FirebaseFirestore.getInstance()
+                            .collection("User")
+                            .document(userId);
+
+                    // Set the name data
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("name", user.getDisplayName());
+
+                    documentReference.set(data).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+
+                                    Intent intent=new Intent(Login_Page.this, Home_Page.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Failed to store name
+                                    Toast.makeText(Login_Page.this, "Something Went Wrong" , Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                }
+            }
+        });
+
+    }
+
+    void loginaccountInFirebase(String email, String password){
         FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
 
         firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(Login_Page.this, new OnCompleteListener<AuthResult>() {
--- app/src/main/java/com/example/pulink/SignUp_Page.java
+++ app/src/main/java/com/example/pulink/SignUp_Page.java
@@ -1,6 +1,7 @@
 package com.example.pulink;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,16 +11,25 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -31,9 +41,17 @@ public class SignUp_Page extends AppCompatActivity {
 
     Button signupbtn;
     String docid;
+    ImageView google;
     EditText email_edittext,password_edittext,conf_password_edittext,name_edittext;
 
     TextView logintext;
+
+
+    // signup using google
+    FirebaseAuth firebaseAuth ;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    int RC_SIGN_IN=20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +64,15 @@ public class SignUp_Page extends AppCompatActivity {
         password_edittext=findViewById(R.id.password_edittext);
         conf_password_edittext=findViewById(R.id.conf_password_edittext);
         name_edittext=findViewById(R.id.name_edittext);
+        google=findViewById(R.id.google);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("51839764002-runfu5jnlf1uqjpcjp9820f68f99qk8a.apps.googleusercontent.com").requestEmail().build();
+        gsc= GoogleSignIn.getClient(this,gso);
+
+
+
+
 
 
 
@@ -74,16 +101,82 @@ public class SignUp_Page extends AppCompatActivity {
             }
         });
 
+
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signinusinggoogle();
+            }
+        });
+
+    }
+
+    void signinusinggoogle(){
+        Intent signIntent=gsc.getSignInIntent();
+        startActivityForResult(signIntent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==RC_SIGN_IN){
+            Task<GoogleSignInAccount>task=GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account=task.getResult(ApiException.class);
+                firebaseauth(account.getIdToken());
+            }
+            catch (Exception e){
+                Toast.makeText(SignUp_Page.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+
+            }
+        }
     }
 
 
+    public void firebaseauth(String idToken){
+        AuthCredential authCredential= GoogleAuthProvider.getCredential(idToken,null);
+        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user=firebaseAuth.getCurrentUser();
+                    String userid=user.getUid();
+
+
+                    DocumentReference documentReference=FirebaseFirestore.getInstance().collection("User").document(userid);
+
+                    Map<String ,Object> hm=new HashMap<>();
+                    hm.put("name",user.getDisplayName());
+
+                    documentReference.set(hm).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Intent intent=new Intent(SignUp_Page.this,Home_Page.class);
+                            startActivity(intent);
+                            finish();
+                        }
+
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SignUp_Page.this,"Something went wrong",Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
     void createaccountInFirebase(String email, String password, String name) {
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUp_Page.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+
                             // User created successfully, now store name in Firestore
                             String userId = firebaseAuth.getCurrentUser().getUid();
                             DocumentReference documentReference = FirebaseFirestore.getInstance()
